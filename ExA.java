7d62ee04320da0d2bf8e3bc8a909824b30c44ca5
IgorRodriguesx <igor.oficial.igor@gmail.com>
2022-09-20 14:57:10 -0300

    exercício A

---
+++ ExA.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.lista2;
+
+/**
+ *
+ * @author igoro
+ */
+
+import javax.swing.JOptionPane;
+
+public class ExA {
+
+    public static void main(String[] args) {
+        
+        double nota1 , nota2 , media   ;
+        nota1 = Double.parseDouble(JOptionPane.showInputDialog("Digite a primeira nota:  "));
+        nota2 = Double.parseDouble(JOptionPane.showInputDialog("Digite a segunda nota:  "));
+        media = (nota1 + nota2)/2;
+        
+            if(media >= 7) {
+                JOptionPane.showMessageDialog(null,"Aprovado com media:  " + media);
+        
+            }else{
+                JOptionPane.showMessageDialog(null,"Reprovado com media:  " + media);
+                
+            }
+        
+        
+    }
+}
